379f0767ef1bad3d831d4b50acaeebfd444b8a94
kouichi shinkokura <stndard809@gmail.com>
2022-11-21 21:12:26 +0900

    goya

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/sample', WEBrick::HTTPServlet::ERBHandler, 'sample.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
